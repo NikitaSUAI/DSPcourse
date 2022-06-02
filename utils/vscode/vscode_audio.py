4f9d1c7cecee6eec328e7724b2e6e3fb1a6349c3
NikitaSUAI <NikittossiiACAB@yandex.ru>
2022-06-03 00:10:33 +0300

    aio

--- utils/vscode/vscode_audio.py
+++ utils/vscode/vscode_audio.py
@@ -13,22 +13,66 @@ def Audio(audio: np.ndarray, rate: int):
     else:
         channels = audio.tolist()
 
-    return IPython.display.HTML("""
+    return IPython.display.HTML(
+        """
         <script>
-            if (!window.audioContext) {
-                window.audioContext = new AudioContext();
-                window.playAudio = function(audioChannels, sr) {
-                    const buffer = audioContext.createBuffer(audioChannels.length, audioChannels[0].length, sr);
-                    for (let [channel, data] of audioChannels.entries()) {
-                        buffer.copyToChannel(Float32Array.from(data), channel);
-                    }
-            
-                    const source = audioContext.createBufferSource();
-                    source.buffer = buffer;
-                    source.connect(audioContext.destination);
-                    source.start();
-                }
+    var source;
+    if (!window.audioContext) {
+        window.audioContext = new AudioContext();
+        window.playAudio = function(audioChannels, sr) {
+            const buffer = audioContext.createBuffer(audioChannels.length, audioChannels[0].length, sr);
+            for (let [channel, data] of audioChannels.entries()) {
+                buffer.copyToChannel(Float32Array.from(data), channel);
             }
-        </script>
-        <button onclick="playAudio(%s, %s)">Play</button>
-    """ % (json.dumps(channels), rate))
+    
+            source = audioContext.createBufferSource();
+            source.buffer = buffer;
+            source.connect(audioContext.destination);
+        }
+        window.audioContext({json.dumps(channels)}, {rate})
+    }
+    window.change_view = function(){
+        but = document.getElementById("ply");
+        if (but.innerHTML == ">"){
+            source.start();
+        }
+        else {
+            source.suspend();
+        }
+        but.innerHTML = but.innerHTML == "||" ? ">":"||";
+    }
+</script>
+<button id="ply" onclick="change_view()">></button>
+<progress value="0", max="3"></progress>
+<input type="range">
+<select title="rate">
+    <option value="8000">8000</option>
+    <option value="8000">11025</option>
+    <option value="8000">22050</option>
+    <option value="8000">44100</option>
+    <option value="8000">48000</option>
+</select>hz
+<button>Download</button>
+        """
+        
+        
+        
+    #     """
+    #     <script>
+    #         if (!window.audioContext) {
+    #             window.audioContext = new AudioContext();
+    #             window.playAudio = function(audioChannels, sr) {
+    #                 const buffer = audioContext.createBuffer(audioChannels.length, audioChannels[0].length, sr);
+    #                 for (let [channel, data] of audioChannels.entries()) {
+    #                     buffer.copyToChannel(Float32Array.from(data), channel);
+    #                 }
+            
+    #                 const source = audioContext.createBufferSource();
+    #                 source.buffer = buffer;
+    #                 source.connect(audioContext.destination);
+    #                 source.start();
+    #             }
+    #         }
+    #     </script>
+    #     <button onclick="playAudio(%s, %s)">Play</button>
+    # """ % (json.dumps(channels), rate))
